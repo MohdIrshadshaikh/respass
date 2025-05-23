5cf3e679bffcfb92ac72a6ef1e020cf9ca109cdd
irshad <mohdirshadshaikh1234@gmail.com>
2025-05-23 19:45:03 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import react from 'react'
+import Nav from "./Components/Nav"
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+<Nav/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+function Nav (){
+    return(
+        <>
+
+        <h1>Navbar</h1>
+        </>
+    )
+}
+
+
+export default Nav
